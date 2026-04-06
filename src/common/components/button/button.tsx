396d57eeb67a8d66b6c7430003cf416dbcc1b9be
dzianis kavaliou <ds.kavaliou@gmail.com>
2026-04-06 14:53:24 +0200

    feat: add ui component

--- src/common/components/button/button.tsx
+++ src/common/components/button/button.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Button as ButtonPrimitive } from '@base-ui/react/button'
 import { cva } from 'class-variance-authority'
 import type { VariantProps } from 'class-variance-authority'

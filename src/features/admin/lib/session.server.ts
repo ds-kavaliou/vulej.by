1f36672f0d9c3d837ce8cfa960150e39d987d9f5
dzianis kavaliou <ds.kavaliou@gmail.com>
2026-06-06 08:31:42 +0200

    fix: check for ADMIN_SESSION_SECRET

--- src/features/admin/lib/session.server.ts
+++ src/features/admin/lib/session.server.ts
@@ -5,9 +5,15 @@ type SessionData = {
 }
 
 export function useAppSession() {
+  const password = process.env.ADMIN_SESSION_SECRET
+
+  if (!password) {
+    throw new Error('ADMIN_SESSION_SECRET environment variable is not set')
+  }
+
   return useSession<SessionData>({
     name: 'admin-session',
-    password: process.env.ADMIN_SESSION_SECRET!,
+    password,
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
